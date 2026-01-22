9e840f03aef28cbc119fde5ae1689f141133b4f9
pschoro <pschoro@gmail.com>
2026-01-22 03:30:47 +0100

    fix

--- client/src/components/MultimediaElement.tsx
+++ client/src/components/MultimediaElement.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, forwardRef } from 'react';
+import { useEffect, useRef, useState, forwardRef } from 'react';
 import { Image as KonvaImage } from 'react-konva';
 import useImage from 'use-image';
 
